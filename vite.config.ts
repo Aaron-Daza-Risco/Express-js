a55d4c5be330978e21a7aa036137bc6f36b54e20
Aaron-Daza-Risco <aarondazarisco@gmail.com>
2024-12-02 15:25:56 -0500

    xd

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Express-js/'
+  base: '/Express-js/', // Asegúrate que coincida exactamente con el nombre de tu repositorio
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false, // Desactivar sourcemaps para producción
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  }
 })
